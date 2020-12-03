690560d7efa58cede97b4f0a3c4a1e32052f16ae
star8kid <2022younganthonyn@aaps.k12.mi.us>
2020-12-02 22:14:53 -0500

    Day One Part One Challenge completed

---
+++ DayOnePartOne.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+// Part One Timestamp: 56 minutes 15 seconds
+
+int expenseCalc(string inputFileName){
+    string fileLine;
+    ifstream inputFile(inputFileName);
+    vector<int> fileIntegerValues = {};
+    if(inputFile.is_open()){
+        while(getline(inputFile,fileLine)){
+            fileIntegerValues.push_back(stoi(fileLine));
+        }
+    }
+    inputFile.close();
+    for(int a = 0; a < fileIntegerValues.size(); a++){
+        for( int b = a; b < fileIntegerValues.size(); b++){
+            if((fileIntegerValues[a] + fileIntegerValues[b]) == 2020){
+                return fileIntegerValues[a] * fileIntegerValues[b];
+            }
+        }
+    }
+}
+
+int main(){
+    cout << expenseCalc("dayOneInput.txt");
+}
