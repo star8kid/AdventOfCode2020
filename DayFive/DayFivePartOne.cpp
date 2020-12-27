35533af1f9f2966a7d6e52df13d42afa48572897
star8kid <2022younganthonyn@aaps.k12.mi.us>
2020-12-27 09:54:34 -0500

    Fix Day Five list calculations

--- DayFive/DayFivePartOne.cpp
+++ DayFive/DayFivePartOne.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<string>
+#include<algorithm>
 
 using namespace std;
 
@@ -28,7 +29,7 @@ int main(){
     int lowestXValue = 0;
     int upperXValue = highestXValue;
     int lowerXValue = lowestXValue;
-    pair <int,int> boundsXValue (lowerXValue, highestXValue);
+    pair <int,int> boundsXValue (lowerXValue, upperXValue);
     // Y Coordinate Definitions
     int highestYValue = 7;
     int lowestYValue = 0;
@@ -37,12 +38,56 @@ int main(){
     pair <int,int> boundsYValue (lowerYValue, upperYValue);
 
     for(int a = 0; a < planeSeatPartitions.size(); a++){
-        for(int b = 0; b < 5; b++){
-            if (planeSeatPartitions[a][b] == 'F'){
-                
+        // For Loop to find the X Value
+        for(int b = 0; b < 7; b++){
+            if(b == 6){
+                if(planeSeatPartitions[a][b] == 'F'){
+                    XValue = boundsXValue.first;
+                }
+                else{
+                    XValue = boundsXValue.second;
+                }
             }
-
+            else{
+                if (planeSeatPartitions[a][b] == 'F'){
+                    // 'Lower Half' case
+                    upperXValue = ((upperXValue + lowerXValue + 1) / 2);
+                }
+                else if(planeSeatPartitions[a][b] == 'B'){
+                    // 'Upper Half' case
+                    lowerXValue = ((upperXValue + lowerXValue + 1) / 2);
+                }
+            }
         }
+        // For Loop to find the Y Value
+        for(int c = 7; c < 10; c++){
+            if(c == 9){
+                if(planeSeatPartitions[a][c] == 'L'){
+                    YValue = boundsYValue.first;
+                }
+                else{
+                    YValue = boundsYValue.second;
+                }
+            }
+            else{
+                if (planeSeatPartitions[a][c] == 'L'){
+                    // 'Lower Half' case
+                    upperYValue = ((upperYValue + lowerYValue + 1) / 2);
+                }
+                else if(planeSeatPartitions[a][c] == 'R'){
+                    // 'Upper Half' case
+                    lowerYValue = ((upperYValue + lowerYValue + 1) / 2);
+                }
+            }
+        }
+        // Seat ID Calculations
+        planeSeatIDS.push_back((planeCoordinates.first * 8) + planeCoordinates.second);
+        // Reset the values for looping again
+        upperXValue = 127;
+        lowerXValue = 0;
+        upperYValue = 7;
+        lowerYValue = 0;
     }
-
+    cout << *max_element(planeSeatIDS.begin(), planeSeatIDS.end()) << endl;
+    return *max(planeSeatIDS.begin(),planeSeatIDS.end());
 }
