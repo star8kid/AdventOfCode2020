0ef33bceac0457c9524b6a1e4b9d00a69af17441
star8kid <2022younganthonyn@aaps.k12.mi.us>
2020-12-29 18:21:25 -0500

    Debugging Day Five Part Two

--- DayFive/DayFivePartTwo.cpp
+++ DayFive/DayFivePartTwo.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-// Full Timelapse: 57 minutes 43 seconds
+// Full Timelapse: 1 hour 54 minutes 55 seconds
 
 // A helper function. Ignore it.
 
@@ -43,15 +43,21 @@ void elementsOfTwoDim(vector<vector<TwoDimElement>> inputVector){
     cout << "]" << endl;
 }
 
+
+
+bool firstElementComp(vector<int> inputVecOne, vector<int> inputVecTwo){
+    return inputVecOne[0] < inputVecTwo[0];
+}
+
 /*
 
     Just to imagine this better, here is a self-note of the type of grid
     I want to create of the plane:
 
     [                
-    [0,2],[1,2],[2,2]
-    [0,1],[1,1],[2,1]
-    [0,0],[1,0],[2,0]   
+        [0,2],[1,2],[2,2]
+        [0,1],[1,1],[2,1]
+        [0,0],[1,0],[2,0]   
     ]
 
 */
@@ -81,7 +87,7 @@ int main(){
     // Values and Coordinate Vector
     int XValue;
     int YValue;
-    vector<vector<int>> planeSeatCoordinates = {};
+    vector< vector<int> > planeSeatCoordinates = {};
 
     for(int a = 0; a < planeSeatPartitions.size(); a++){
         // For Loop to find the X Value
@@ -131,22 +137,49 @@ int main(){
         cout << "The Current X Value is: " << XValue << endl;
         cout << "The Current Y Value is: " << YValue << endl;
         elementsOf(seatCoordinates);
+        cout << "The length of this element vector in the vector container is: " << planeSeatCoordinates[a].size() << endl;
         planeSeatCoordinates.push_back(seatCoordinates);
+        cout << "This has successfully ran" << endl;
         // Reset the values for looping again
         upperXValue = 127;
         lowerXValue = 0;
         upperYValue = 7;
         lowerYValue = 0;
+        // Potential Reset Fix?
+        XValue = 0;
+        YValue = 0;
     }
 
+
+
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << "The length of this element vector in the vector container is: " << planeSeatCoordinates[0].size() << endl;
+    cout << "This is that vector:" << endl;
+    elementsOf(planeSeatCoordinates[0]);
+
+    // BFFFBBFRRR
+    // FFFBBBFRRR
+    // BBFFBBFRLL
+
+
+    // elementsOfTwoDim(planeSeatCoordinates);
+    
     // vector<vector<int>> twoDimTestVec = {};
     // vector<int> testVecOne = {1,5,7};
     // vector<int> testVecTwo = {2,9,1};
     // vector<int> testVecThree = {3,4,6};
+    // twoDimTestVec.push_back(testVecThree);
     // twoDimTestVec.push_back(testVecOne);
     // twoDimTestVec.push_back(testVecTwo);
-    // twoDimTestVec.push_back(testVecThree);
 
+    // cout << "Before the first element sort:" << endl;
+    // elementsOfTwoDim(twoDimTestVec);
+
+    // sort(twoDimTestVec.begin(), twoDimTestVec.end(), firstElementComp);
+
+    // cout << "After the first element sort:" << endl;
     // elementsOfTwoDim(twoDimTestVec);
 
     elementsOfTwoDim(planeSeatCoordinates);
