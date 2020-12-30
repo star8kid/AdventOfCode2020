c4a6170083b4f1700a0d4b01c7e4169a958d941f
star8kid <2022younganthonyn@aaps.k12.mi.us>
2020-12-30 15:26:58 -0500

    Solved DAY FIVE FINALLLLLLLLLLYYYYY YESSSS-

--- DayFive/DayFivePartTwo.cpp
+++ DayFive/DayFivePartTwo.cpp
@@ -6,10 +6,9 @@
 
 using namespace std;
 
-// Full Timelapse: 1 hour 54 minutes 55 seconds
+// Full Timelapse: 2 hours 56 minutes 48 seconds
 
 // A helper function. Ignore it.
-
 template <class Element>
 
 void elementsOf(vector<Element> inputVector){
@@ -31,8 +30,8 @@ void elementsOfTwoDim(vector<vector<TwoDimElement>> inputVector){
     cout << "The elements of this two dimensional vector are:" << endl << "[" << endl;
     for (int a = 0; a < inputVector.size(); a++){
         cout << "   [";
-        for (int b = 0; b < inputVector.size(); b++){
-            if(b == inputVector.size() - 1){
+        for (int b = 0; b < inputVector[a].size(); b++){
+            if(b == inputVector[a].size() - 1){
                 cout << inputVector[a][b] << "]" << endl;
             }
             else{
@@ -42,26 +41,6 @@ void elementsOfTwoDim(vector<vector<TwoDimElement>> inputVector){
     }
     cout << "]" << endl;
 }
-
-
-
-bool firstElementComp(vector<int> inputVecOne, vector<int> inputVecTwo){
-    return inputVecOne[0] < inputVecTwo[0];
-}
-
-/*
-
-    Just to imagine this better, here is a self-note of the type of grid
-    I want to create of the plane:
-
-    [                
-        [0,2],[1,2],[2,2]
-        [0,1],[1,1],[2,1]
-        [0,0],[1,0],[2,0]   
-    ]
-
-*/
-
 
 int main(){
 
@@ -77,17 +56,16 @@ int main(){
             planeSeatPartitions.push_back(tempString);
         }
     }
-
     // X Coordinate Definitions
     int upperXValue = 127;
     int lowerXValue = 0;
     // Y Coordinate Definitions
     int upperYValue = 7;
     int lowerYValue = 0;
-    // Values and Coordinate Vector
+    // Values and ID Vector
     int XValue;
     int YValue;
-    vector< vector<int> > planeSeatCoordinates = {};
+    vector<int> planeSeatIDS = {};
 
     for(int a = 0; a < planeSeatPartitions.size(); a++){
         // For Loop to find the X Value
@@ -132,56 +110,31 @@ int main(){
                 }
             }
         }
-        // Seat Coordinates Storing 
-        vector<int> seatCoordinates = {XValue,YValue};
-        cout << "The Current X Value is: " << XValue << endl;
-        cout << "The Current Y Value is: " << YValue << endl;
-        elementsOf(seatCoordinates);
-        cout << "The length of this element vector in the vector container is: " << planeSeatCoordinates[a].size() << endl;
-        planeSeatCoordinates.push_back(seatCoordinates);
-        cout << "This has successfully ran" << endl;
+        // Seat ID Calculations
+        pair <int,int> planeCoordinates(XValue,YValue);
+        planeSeatIDS.push_back((planeCoordinates.first * 8) + planeCoordinates.second);
         // Reset the values for looping again
         upperXValue = 127;
         lowerXValue = 0;
         upperYValue = 7;
         lowerYValue = 0;
-        // Potential Reset Fix?
-        XValue = 0;
-        YValue = 0;
     }
 
+    sort(planeSeatIDS.begin(), planeSeatIDS.end());
 
+    // I'm thinking that one ID element that is two away from the next one instead
+    // of a single value away shows that that's the boarding pass missing: my seat id.
 
-    cout << endl;
-    cout << endl;
-    cout << endl;
-    cout << "The length of this element vector in the vector container is: " << planeSeatCoordinates[0].size() << endl;
-    cout << "This is that vector:" << endl;
-    elementsOf(planeSeatCoordinates[0]);
-
-    // BFFFBBFRRR
-    // FFFBBBFRRR
-    // BBFFBBFRLL
-
-
-    // elementsOfTwoDim(planeSeatCoordinates);
-    
-    // vector<vector<int>> twoDimTestVec = {};
-    // vector<int> testVecOne = {1,5,7};
-    // vector<int> testVecTwo = {2,9,1};
-    // vector<int> testVecThree = {3,4,6};
-    // twoDimTestVec.push_back(testVecThree);
-    // twoDimTestVec.push_back(testVecOne);
-    // twoDimTestVec.push_back(testVecTwo);
-
-    // cout << "Before the first element sort:" << endl;
-    // elementsOfTwoDim(twoDimTestVec);
-
-    // sort(twoDimTestVec.begin(), twoDimTestVec.end(), firstElementComp);
-
-    // cout << "After the first element sort:" << endl;
-    // elementsOfTwoDim(twoDimTestVec);
-
-    elementsOfTwoDim(planeSeatCoordinates);
-
+    vector<vector<int>> potentialAdjSeatIdsPairVector = {};
+    vector<int> potentialAdjSeatIds = {};
+    for (int d = 0; d < planeSeatIDS.size(); d++){
+        if ((planeSeatIDS[d] + 2) == planeSeatIDS[d + 1]){
+            potentialAdjSeatIds.push_back(planeSeatIDS[d]);
+            potentialAdjSeatIds.push_back(planeSeatIDS[d + 1]);
+            potentialAdjSeatIdsPairVector.push_back(potentialAdjSeatIds);
+            potentialAdjSeatIds.clear();
+        }
+    }
+    cout << "My seat ID is the number in between the two numbers of the vector below:" << endl;
+    elementsOfTwoDim(potentialAdjSeatIdsPairVector);
 }
